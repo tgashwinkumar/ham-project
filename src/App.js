a39158d760e88fa699d6eb98b01bf3c6e624a924
tgashwinkumar <tgashwinkumar@gmail.com>
2020-12-28 06:24:15 +0530

    Resolved framer-motion.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import './App.css';
-import NavBar from './components/NavBar';
+import {React} from 'react'
+import {motion} from 'framer-motion'
+// import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      {/* <NavBar /> */}
+      <Home/>
     </div>
   );
+}
+
+const Home = () => {
+  return (
+    <div className = "home container">
+      <motion.h2 animate = {{ fontSize: 50, color: "#000000", x: 100, y: 100}}>
+        Hari, Ashwin, Manu
+      </motion.h2>
+    </div>
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { react } from 'react';
-import { Link } from 'react-router-dom';
-import { motion } from 'framer-motion'; 
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,15 +11,6 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-const Home = () => {
-  return (
-    <div className = "home container">
-      <motion.h2 animate = {{ fontSize: 50, color: "#ffffff", x: 100, y: 100}}>
-        Hari, Ashwin, Manu
-      </motion.h2>
-    </div>
-  )
-}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
