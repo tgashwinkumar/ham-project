bd8f38dabb2089aa0d5b9c14541cc5ada4c76015
tgashwinkumar <tgashwinkumar@gmail.com>
2020-12-30 02:04:13 +0530

    Inserted Routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
-import {React} from 'react'
+import React from 'react'
+import { Route, Switch } from 'react-router';
+import Home from './pages/Home'
+import Documents from './pages/Documents'
+import Statistics from './pages/Statistics';
+import AboutUs from './pages/AboutUs'
 
 function App() {
+
+  const routes = [
+    {path:'/', component: Home},
+    {path:'/documents', component: Documents},
+    {path:'/statistics', component: Statistics},
+    {path:'/aboutus', component: AboutUs},
+  ]
+
   return (
     <div className="App">
-      
+      <Switch>
+        {
+          routes.map(route => <Route exact path={route.path} component={route.component}/>)
+        }
+      </Switch>
     </div>
   );
 }
