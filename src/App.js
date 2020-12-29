89144c2837655ef0c8673843bf4169468d00e8d9
tgashwinkumar <tgashwinkumar@gmail.com>
2020-12-29 21:26:28 +0530

    Cleaned the repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import './App.css';
 import {React} from 'react'
-import {motion} from 'framer-motion'
-// import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      {/* <NavBar /> */}
-      <Home/>
+      
     </div>
   );
-}
-
-const Home = () => {
-  return (
-    <div className = "home container">
-      <motion.h2 animate = {{ fontSize: 50, color: "#000000", x: 100, y: 100}}>
-        Hari, Ashwin, Manu
-      </motion.h2>
-    </div>
-  )
 }
 
 export default App;
