e6fe31d8be59a80937d1f3697635de2a4d784760
tgashwinkumar <tgashwinkumar@gmail.com>
2020-12-30 13:38:38 +0530

    Started Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,10 +2,17 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div>
-            Home Page
-        </div>
+        <>
+            <div className="hover-container" style={HoverContainerStyles}></div>
+            <div className="container">
+
+            </div>
+        </>
     )
 }
 
 export default Home
+
+const HoverContainerStyles = {
+    
+}
