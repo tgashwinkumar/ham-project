f0610750242fef1954ee4b5cd40a2e733942be1c
tgashwinkumar <tgashwinkumar@gmail.com>
2020-12-31 03:00:42 +0530

    Made few changes on hoverContainer and Added logo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,25 @@
 import React from 'react'
 import '../styles/Home.css'
+import ChakraIcon from '../assets/chakra-icon.png'
 
 const Home = () => {
     return (
-        <>
-            <div className="container">
-
-            </div>
-        </>
+        <div className="home-container">
+            <nav className="home-header">
+                <img className="home-chakra-icon" src={ChakraIcon} alt="Ashoka Chakra Icon"/>
+                <div className="home-title">
+                    <span>Ministry of Lorem Ipsum</span>
+                    <span>Government of Lorem Ipsum</span>
+                </div>
+                <div className="home-spacer"></div>
+                <div className="home-nav">
+                    <span>About Us</span>
+                    <span>Documents</span>
+                    <span>Statistics</span>
+                </div>
+            </nav>
+        </div>
     )
 }
-
+    
 export default Home
