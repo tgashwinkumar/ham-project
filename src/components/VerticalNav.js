2910a0745c56786ef93079dc04600ae7ba3f83c7
tgashwinkumar <tgashwinkumar@gmail.com>
2021-01-02 01:15:50 +0530

    Changes to util.js

--- src/components/VerticalNav.js
+++ src/components/VerticalNav.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import {statNav} from '../utils/utils'
+import BarsIcon from '../assets/menu.svg'
+import '../styles/VerticalNav.css'
 
 const VerticalNav = () => {
     return (
-        <div>
-            asd
-        </div>
+        <React.Fragment>
+            <img src={BarsIcon} alt="bars-icon" className="bars-icon"/>
+            <nav className="statis-nav-container">
+                {statNav.map(statLink => {
+                    return <div className="statis-nav-link">
+                        <img src={statLink.icon} alt={`${statLink.name}-icon`}/>
+                        <span>{statLink.name}</span>
+                    </div>
+                })}
+            </nav>
+        </React.Fragment>
     )
 }
 
