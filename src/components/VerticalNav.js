4c9c15c131187cf0a9adccf83ca8c7ce0c7aac2f
tgashwinkumar <tgashwinkumar@gmail.com>
2021-01-02 12:00:05 +0530

    Finished nav (1)

--- src/components/VerticalNav.js
+++ src/components/VerticalNav.js
@@ -1,21 +1,30 @@
-import React from 'react'
-import {statNav} from '../utils/utils'
+import React, { useState } from 'react'
 import BarsIcon from '../assets/menu.svg'
+import PinIcon from '../assets/pin.svg'
 import '../styles/VerticalNav.css'
 
-const VerticalNav = () => {
+const VerticalNav = ({title, navData}) => {
+
+    const [navActive, setNavActive] = useState(false)
+
     return (
-        <React.Fragment>
-            <img src={BarsIcon} alt="bars-icon" className="bars-icon"/>
-            <nav className="statis-nav-container">
-                {statNav.map(statLink => {
-                    return <div className="statis-nav-link">
-                        <img src={statLink.icon} alt={`${statLink.name}-icon`}/>
-                        <span>{statLink.name}</span>
-                    </div>
+        <div className={`nav-container ${navActive?'nav-container-active':''}`} onClick={() => setNavActive(!navActive)}>
+            <div className="nav-header">
+                <img src={BarsIcon} alt="bars-icon" className={`bars-icon ${navActive?'bars-icon-active':''}`}/>
+                <p>{title}</p>
+            </div>
+            <nav className="nav-menu">
+                {navData.map(navLink => {
+                    return <>
+                        <div className="nav-link">
+                            <img src={navLink.icon || PinIcon} alt={`${navLink.name}-icon`}/>
+                            <span>{navLink.name}</span>
+                        </div>
+                        <hr/>
+                    </>
                 })}
             </nav>
-        </React.Fragment>
+        </div>
     )
 }
 
--- src/pages/Statistics.js
+++ src/pages/Statistics.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import VerticalNav from '../components/VerticalNav'
+import {statNav} from '../utils/utils'
 
 const Statistics = () => {
-
-
     return (
         <React.Fragment>
-            <VerticalNav />
+            <VerticalNav title="Statistics" navData={statNav}/>
         </React.Fragment>
     )
 }
